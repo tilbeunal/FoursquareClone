91f90c865be56247f3e69d76b2ceed3f55149a51
tilbeunalsengezer <tilbeunal@gmail.com>
2023-01-06 22:23:25 +0300

    Finish Project

---
+++ FoursquareClone/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  FoursquareClone
+//
+//  Created by Tilbe Ünal on 3.12.2022.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "S5mgmwb3Jp0CiUrUv2U5bky550YIHG4K32fvWphx"
+            ParseMutableClientConfiguration.clientKey = "JgtrXMSTTz5LsGQhRoSqSpVuog7IgVGpqz6WgD46"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+            
+        Parse.initialize(with: configuration)
+        
+        return true
+        
+        }
+        
+
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ FoursquareClone/PlaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceViewController.swift
+//  FoursquareClone
+//
+//  Created by Tilbe Ünal on 15.12.2022.
+//
+
+import UIKit
+
+class PlaceViewController: UIViewController {
+    var isShowImageView = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
